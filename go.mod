a4bcc4757d77fe7c08f63b010c7f0229db5a09ca
ynsluhn <ynsluhan@foxmail.com>
2022-07-10 00:19:27 +0800

    update

--- go.mod
+++ go.mod
@@ -4,10 +4,11 @@ go 1.16
 
 require (
 	github.com/antonfisher/nested-logrus-formatter v1.3.1
-	github.com/gin-gonic/gin v1.7.4
+	github.com/gin-gonic/gin v1.8.1
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
 	github.com/lestrrat-go/strftime v1.0.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
 	github.com/sirupsen/logrus v1.8.1
+	github.com/ynsluhan/go-config v1.1.1 // indirect
 )
--- logger.go
+++ logger.go
@@ -4,32 +4,39 @@ import (
 	nested "github.com/antonfisher/nested-logrus-formatter"
 	"github.com/gin-gonic/gin"
 	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
-	"github.com/rifflock/lfshook"
 	"github.com/sirupsen/logrus"
+	config "github.com/ynsluhan/go-config"
 	"log"
 	"os"
 	"path"
+	"strconv"
+	"time"
 )
 
-/**
- * @Author yNsLuHan
- * @Description:
- * @Time 2021-06-28 09:20:20
- */
-func InitJsonLogger(logPath *string) {
+var conf *config.Config
+
+func init() {
 	//
-	var lp string
-	if logPath == nil {
-		lp = GetLogPath()
-	} else {
-		lp = *logPath
+	if conf.Server.EnableLogger {
+		initLogger()
 	}
-	// 日志文件
-	fileName := path.Join(lp, "bo.log")
+}
+
+func initLogger() {
+	var dir = conf.Server.LoggerDir
+	var fileName string
+	//
+	if len(dir) == 0 {
+		getwd, _ := os.Getwd()
+		dir = path.Join(getwd, "logs")
+	} else {
+		// 日志文件
+		fileName = path.Join(dir, "bo.log")
+	}
 	// 写入文件
 	src, err := os.OpenFile(fileName, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatal("ERROR 日志文件创建失败:", err)
+		log.Fatal("[logger] ERROR 日志文件创建失败:", err)
 	}
 	// 实例化
 	logger = logrus.New()
@@ -37,7 +44,7 @@ func InitJsonLogger(logPath *string) {
 	logger.Out = src
 	// 设置日志级别
 	logger.SetLevel(logrus.InfoLevel)
-	// 设置普通文本日志输出模式  json模式注释
+	// 设置普通日志输出模式  json模式注释
 	logger.SetFormatter(
 		&nested.Formatter{
 			// 禁用键值对日志类型
@@ -50,11 +57,10 @@ func InitJsonLogger(logPath *string) {
 			NoColors: true,
 		},
 	)
-
 	// 设置 rotatelogs
 	logWriter, err := rotatelogs.New(
 		// 分割后的文件名称
-		path.Join(lp, "bo-%Y-%m-%d_%H.log"),
+		path.Join(dir, "bo-%Y-%m-%d_%H.log"),
 		// 生成软链，指向最新日志文件
 		rotatelogs.WithLinkName(fileName),
 		// 设置最大保存时间(30天)
@@ -62,28 +68,12 @@ func InitJsonLogger(logPath *string) {
 		// 设置日志切割时间间隔(1天)
 		rotatelogs.WithRotationTime(24*time.Hour),
 	)
-
-	// json格式  普通文本日志模式注释
-	writeMap := lfshook.WriterMap{
-		logrus.InfoLevel:  logWriter,
-		logrus.FatalLevel: logWriter,
-		logrus.DebugLevel: logWriter,
-		logrus.WarnLevel:  logWriter,
-		logrus.ErrorLevel: logWriter,
-		logrus.PanicLevel: logWriter,
-	}
-
-	// 钩子函数，格式化日期 json格式 普通模式注释
-	lfHook := lfshook.NewHook(writeMap, &logrus.JSONFormatter{
-		TimestampFormat: "2006-01-02 15:04:05",
-	})
-
-	// 新增 Hook json格式 普通模式注释
-	logger.AddHook(lfHook)
+	// 普通写入设置日志切割  json模式注释
+	logger.SetOutput(logWriter)
 }
 
 // 使用日志框架logrus日志记录到文件
-func JsonLogger() gin.HandlerFunc {
+func DefaultLogger() gin.HandlerFunc {
 	// 使用logrus 日志系统
 	return func(c *gin.Context) {
 		// 开始时间
@@ -99,16 +89,8 @@ func JsonLogger() gin.HandlerFunc {
 		statusCode := c.Writer.Status()
 		// 请求IP
 		clientIP := c.ClientIP()
-		// 日志格式
-		logger.WithFields(logrus.Fields{
-			"req_method":   reqMethod,
-			"status_code":  statusCode,
-			"req_uri":      reqUri,
-			"latency_time": cost,
-			//"agent:":       c.Request.UserAgent(),
-			"client_ip": clientIP,
-		}).Info()
+		// 自定义日志
+		logger.Info(clientIP+" ", reqMethod+" ", reqUri+" ", strconv.Itoa(statusCode)+" ", cost)
 		c.Next()
 	}
 }
-
